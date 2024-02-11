e3b01f47d7487e6668208c78076f5c89757a99b3
Carson McMahan <carson.mcmahan@gmail.com>
2024-02-11 14:02:13 -0700

    Added Zoom In and Out

--- Assets/Scripts/ButtonPress.cs
+++ Assets/Scripts/ButtonPress.cs
@@ -1,12 +1,15 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Android.Gradle;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ButtonPress : MonoBehaviour
 {
     public GameObject[] TestOnOff;
+    public Animator panelAnimator;
+
     //public GameObject TestOnOff;
     void Start()
     {
@@ -18,10 +21,26 @@ public class ButtonPress : MonoBehaviour
         TestOnOff[4].SetActive(false);
     }
 
+    IEnumerator Waiter(float seconds, int element)
+    {
+        yield return new WaitForSeconds(seconds);
+
+        TestOnOff[element].SetActive(!TestOnOff[element].activeSelf);
+    }
+
     public void SendDebugLog(int element)
     {
         Debug.Log("button was clicked");
         //TestOnOff.SetActive(!TestOnOff.activeSelf);
         TestOnOff[element].SetActive(!TestOnOff[element].activeSelf);
+
+        panelAnimator.SetTrigger("zoomIn");
+    }
+
+    public void BackButton(int element)
+    {
+        panelAnimator.SetTrigger("zoomOut");
+
+        StartCoroutine(Waiter(1, element));
     }
 }
